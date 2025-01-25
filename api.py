b6b6664492392a57796bb65bab9c761cbcca5f32
misam-cybersurf <misamraza@fahm-technologies.com>
2025-01-25 16:34:46 +0530

    implement changes

--- api.py
+++ api.py
@@ -579,13 +579,6 @@ def extract_invoice_detailed(file):
                     pass
             if "Invoice Total" in line :
                 invoice_details["invoice_total"] = line.split(" ")[-1].replace('$','').replace(',', '')
-            if 'Due Date' in line:
-                dt = line.split('\n')
-                invoice_details["due_date"] = dt[0]
-                # Match the date after 'Due Date'
-                # match = re.search(r'Due Date\s*[:\s]*([\d/]+)', line)
-                # if match:
-                    # invoice_details['due_date'] = match.group(1)
 
     def filter_qty_ship(data):
         data = data.split(" ")
@@ -629,34 +622,17 @@ def extract_invoice_detailed(file):
                     # "qty_ship":(filter_qty_ship(data[i+2]) if " " not in data[i+1] else (data[i+1]).split(" ")[0]),
                     "qty_ord": safe_float(data[i + 1].split(" ")[0] if " " in data[i + 1] else data[i + 1]),
                     "qty_ship": safe_float(data[i + 2].split(" ")[0] if " " in data[i + 2] else data[i + 2]),
-                    "unit": (filter_unit(data[i + 3]) if len(data[i + 3].split(" ")) != 1 else data[i + 3])if " " not in data[i + 1] else data[i + 3].split(" ")[-1],
-                    
-                    "item_description": (filter_out_pack(data[i + 4]) if " " not in data[i + 1] else filter_out_pack(data[i + 3])) or filter_out_pack(data[i + 3]),
-                    "size": (filter_out_size(data[i + 4]) if " " not in data[i + 1] else filter_out_size(data[i + 3])) or filter_out_size(data[i + 3]),
-                    "brand": (data[i + 5] if len(data[i + 3].split(" ")) != 1 else data[i + 5]) 
-                        if " " not in data[i + 1] else data[i + 5],
-
-                    "category": (data[i + 7] if len(data[i + 3].split(" ")) != 1 else data[i + 7]) 
-                                if " " not in data[i + 1] else data[i + 7],
-                    "tax": (data[i + 8] if len(data[i + 3].split(" ")) != 1 else data[i + 8]) 
-                        if " " not in data[i + 1] else data[i + 8],
-                    "extended_value": (data[i + 9] if len(data[i + 3].split(" ")) != 1 else data[i + 9]) 
-                                    if " " not in data[i + 1] else data[i + 9],
-                    
-                    # Keep spec unchanged as it's not mapped in your description
-                    "spec": (data[i + 10] if len(data[i + 3].split(" ")) != 1 else data[i + 10]) 
-                            if " " not in data[i + 1] else data[i + 10],
-
-                    # "pack":(filter_out_pack(data[i+4]) if " " not in data[i+1] else filter_out_pack(data[i+3])) or filter_out_pack(data[i+3]),
-                    # "size":(filter_out_size(data[i+4]) if " " not in data[i+1] else filter_out_size(data[i+3])) or filter_out_size(data[i+3]),
-                    # "brand":(data[i+5] if len(data[i+3].split(" "))!=1 else data[i+5]) if " " not in data[i+1] else data[i+5],
-                    # "item_description":(data[i+6] if len(data[i+3].split(" "))!=1 else data[i+6]) if " " not in data[i+1] else data[i+6],
-                    # "category":(data[i+7] if len(data[i+3].split(" "))!=1 else data[i+7]) if " " not in data[i+1] else data[i+7],
-                    # "invent_value":(data[i+8] if len(data[i+3].split(" "))!=1 else data[i+8]) if " " not in data[i+1] else data[i+8],
-                    # "unit_price":(data[i+9] if len(data[i+3].split(" "))!=1 else data[i+9]) if " " not in data[i+1] else data[i+9],
-                    # "spec":(data[i+10] if len(data[i+3].split(" "))!=1 else data[i+10]) if " " not in data[i+1] else data[i+10],
-                    # "tax":(data[i+11] if len(data[i+3].split(" "))!=1 else data[i+11]) if " " not in data[i+1] else data[i+11],
-                    # "extended_value":(data[i+12] if len(data[i+3].split(" "))!=1 else data[i+12]) if " " not in data[i+1] else data[i+12],
+                    "unit":(filter_unit(data[i+2]) if len(data[i+3].split(" "))!=1 else data[i+3]) if " " not in data[i+1] else data[i+2].split(" ")[-1],
+                    "pack":(filter_out_pack(data[i+4]) if " " not in data[i+1] else filter_out_pack(data[i+3])) or filter_out_pack(data[i+3]),
+                    "size":(filter_out_size(data[i+4]) if " " not in data[i+1] else filter_out_size(data[i+3])) or filter_out_size(data[i+3]),
+                    "brand":(data[i+4] if len(data[i+3].split(" "))!=1 else data[i+5]) if " " not in data[i+1] else data[i+4],
+                    "item_description":(data[i+5] if len(data[i+3].split(" "))!=1 else data[i+6]) if " " not in data[i+1] else data[i+5],
+                    "category":(data[i+6] if len(data[i+3].split(" "))!=1 else data[i+7]) if " " not in data[i+1] else data[i+6],
+                    "invent_value":(data[i+7] if len(data[i+3].split(" "))!=1 else data[i+8]) if " " not in data[i+1] else data[i+7],
+                    "unit_price":(data[i+8] if len(data[i+3].split(" "))!=1 else data[i+9]) if " " not in data[i+1] else data[i+8],
+                    "spec":(data[i+9] if len(data[i+3].split(" "))!=1 else data[i+10]) if " " not in data[i+1] else data[i+9],
+                    "tax":(data[i+10] if len(data[i+3].split(" "))!=1 else data[i+11]) if " " not in data[i+1] else data[i+10],
+                    "extended_value":(data[i+11] if len(data[i+3].split(" "))!=1 else data[i+12]) if " " not in data[i+1] else data[i+11],
                 }
                 
                 parsed_items.append(item)
