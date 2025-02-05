e7fe33f9e7771cac856a77082af1b4d00ac71098
misam-cybersurf <misamraza@fahm-technologies.com>
2025-02-05 12:36:45 +0530

    implement storename changes

--- api.py
+++ api.py
@@ -983,6 +983,7 @@ def extract_invoice_detailed(file):
         text = first_page.extract_text()
     
         # Extract invoice number and date
+        location_match = re.search(r'(\d+)\s*- Watikinsville', text)
         invoice_details["store_name"] = location_match.group(1) if location_match else "Not Found"
         for line in text.split('\n'):
             if 'Invoice Date' in line:
@@ -1159,6 +1160,7 @@ def extract_invoice_Sysco(file):
     with pdfplumber.open(file) as pdf:
         first_page = pdf.pages[-1]
         text = first_page.extract_text()
+        location_match = re.search(r'(\d+)\s*- Watikinsville', text)
         invoice_details["store_name"] = location_match.group(1) if location_match else "Not Found"
         # print(text)
         for line in text.split('\n'):
